4050d23e4e7155592262f44f831fb2d1f60a346f
Helder <heldercostaaa@gmail.com>
2020-06-16 15:50:11 -0300

    Add post repository route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,23 +10,38 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get("/repositories", (req, res) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  if (!(title || url || techs))
+    return res.status(400).json({ error: "Missing required params." });
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
+});
+
+app.put("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
